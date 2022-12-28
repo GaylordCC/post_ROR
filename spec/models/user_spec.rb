0b7d411d26d5e1072b9bc7359b813e41ba65ef44
Gaylord Carrillo C <gcarricaballero3@hotmail.com>
2022-12-27 19:13:22 -0500

    implementation of user and post models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validation presence of required fields" do
+    should validate_presence_of(:email)
+    should validate_presence_of(:name)
+    should validate_presence_of(:auth_token)
+  end
+
+  it "validate relation" do
+    should have_many(:posts)
+  end
 end
