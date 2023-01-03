44b1c95fb07e328a3ca44dbdb211b7dcaaf33638
Gaylord Carrillo C <gcarricaballero3@hotmail.com>
2023-01-03 10:57:53 -0500

    Creation a services folder, to implemente the class PostsSearch.Service

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
     def index
         @posts = Post.where(published: true)
         if !params[:search].nil? && params[:search].present?
-            @posts = PostsSearchService.service(@posts, params[:search])
+            @posts = PostsSearchService.search(@posts, params[:search])
         end
         render json: @posts, status: :ok
     end
